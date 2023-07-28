a719db6bfe3066c7b914b7226e7233bf8736943b
banjorabbit <kristiantimm@gmail.com>
2023-07-28 20:55:54 +0200

    cleanup

--- apps/spectrogramzer/src/main.cpp
+++ apps/spectrogramzer/src/main.cpp
@@ -8,35 +8,30 @@ using namespace Eigen;
 using namespace matplot;
 
 // set constants
-enum Languages {EN, DE, ES, FR};
+constexpr int N_FRAMES_MAX = 4*560;
+constexpr int N_BINS_MAX = 4*420;
 constexpr int VERSION = 1;
+
 int main(int argc, char** argv)
 {
-    const std::unordered_map<std::string, Languages> languages{
-      {"en", EN},
-      {"de", DE},
-      {"es", ES},
-      {"fr", FR},
-  };
-
-    cxxopts::Options options(*argv, "A program for creating spectrograms of audio files.");
+  cxxopts::Options options(*argv, "A program for creating spectrograms of audio files.");
     
-    std::string inputName;
-    std::string outputName;
-    float hopSizeMilliseconds;
-    float spectrumSizeMilliseconds;
+  std::string inputName;
+  std::string outputName;
+  float hopSizeMilliseconds;
+  float spectrumSizeMilliseconds;
 
-    options.add_options()
-    ("h,help", "Show help")
-    ("v,version", "Print the current version number")
-    ("i,input", "Name of input file", cxxopts::value(inputName)->default_value("input.wav"))
-    ("o,output", "Name of output file", cxxopts::value(outputName)->default_value("output"))
-    ("t,hop_size", "Size of each time hop between spectrums in milliseconds", cxxopts::value(hopSizeMilliseconds)->default_value("10.f"))
-    ("s,spectrum_size", "Size of each frame used for calculating the spectrum in milliseconds", cxxopts::value(spectrumSizeMilliseconds)->default_value("40.f"));
+  options.add_options()
+  ("h,help", "Show help")
+  ("v,version", "Print the current version number")
+  ("i,input", "Name of input file", cxxopts::value(inputName)->default_value("input.wav"))
+  ("o,output", "Name of output file", cxxopts::value(outputName)->default_value("output.png"))
+  ("t,hop_size", "Size of each time hop between spectrums in milliseconds", cxxopts::value(hopSizeMilliseconds)->default_value("10.f"))
+  ("s,spectrum_size", "Size of each frame used for calculating the spectrum in milliseconds", cxxopts::value(spectrumSizeMilliseconds)->default_value("80.f"));
   
-    options.allow_unrecognised_options();
+  options.allow_unrecognised_options();
     
-    auto result = options.parse(argc, argv);
+  auto result = options.parse(argc, argv);
 
   if (result["help"].as<bool>()) {
     std::cout << options.help() << std::endl;
@@ -52,39 +47,53 @@ int main(int argc, char** argv)
   audioFileInput.load(inputName);
   std::cout << "Input file summary:\n";
   audioFileInput.printSummary();
+  std::cout << "\n";
 
+  std::cout << "Processing summary:\n";
   auto c = Spectrogram::Coefficients();
   c.bufferSize = static_cast<int>(hopSizeMilliseconds / 1000.f * audioFileInput.getSampleRate());
+  std::cout << "Buffer size: " << c.bufferSize << "\n";
   float fftSize = spectrumSizeMilliseconds / 1000.f * audioFileInput.getSampleRate();
   c.fftSize = static_cast<int>(std::pow(2, std::roundf(std::log2f(fftSize))));
+  std::cout << "FFT size: " << c.fftSize << "\n";
   Spectrogram spectrogram(c);
   
   int nFrames = audioFileInput.getNumSamplesPerChannel() / c.bufferSize;
-  //std::vector<std::vector<float>> spec(nFrames, std::vector<float>(c.fftSize/2 + 1));
-  ArrayXXf spec(c.fftSize/2+1, nFrames);
+  std::cout << "Number of frames: " << nFrames << "\n";
+  int nBins = c.fftSize/2+1;
+  ArrayXXf spec(nBins, nFrames);
 
   for (auto nFrame = 0; nFrame < nFrames; nFrame++)
   {
     ArrayXf frameIn = Map<ArrayXf>(&audioFileInput.samples[0][nFrame * c.bufferSize], c.bufferSize);
-    //ArrayXf frameOut = Map<ArrayXf>(&(spec[nFrame][0]), c.fftSize/2+1);
     spectrogram.process(frameIn, spec.col(nFrame));
-    //Map<ArrayXf>(&(spec[nFrame][0]), c.fftSize/2+1) = 20.f * Map<ArrayXf>(&(spec[nFrame][0]), c.fftSize/2+1).log10();
-    //frameOut = 20 * frameOut.log10();
   }
   
-  std::vector<std::vector<float>> specVec(c.fftSize/2 + 1, std::vector<float>(nFrames));
-  for (auto i = 0; i < (c.fftSize/2+1); i++)
+  int nFramesDecimate = std::max(nFrames / N_FRAMES_MAX, 1);
+  int nBinsDecimate = std::max(nBins / N_BINS_MAX, 1);
+  int nFramesPlot = nFrames / nFramesDecimate;
+  int nBinsPlot = nBins / nBinsDecimate;
+
+  std::vector<std::vector<float>> specVec(nBinsPlot, std::vector<float>(nFramesPlot));
+  float maxValue = -100;
+  for (auto i = 0; i < nBinsPlot; i++)
   {
-    for (auto j = 0; j < nFrames; j++)
+    for (auto j = 0; j < nFramesPlot; j++)
     {
-      specVec[i][j] = 20.f * std::log10(spec(i,j));
+      
+      specVec[i][j] = 20.f * std::log10(spec.block(nBinsDecimate * i, nFramesDecimate * j, nBinsDecimate, nFramesDecimate).maxCoeff());
+      maxValue = std::max(maxValue, specVec[i][j]);
     }
   }
-  
+  gca()->color_box_range(maxValue-90, maxValue - 10);
+  gcf()->height(nBinsPlot);
+  gcf()->width(nFramesPlot);
+  gca()->xlabel("Frame number");
+  gca()->ylabel("Frequency bin");
+
   imagesc(specVec);
-  gca()->color_box_range(-80, 20);
   colorbar();
-  save(outputName, "jpeg"); 
+  save(outputName); 
 
   std::cout << "DONE!\n" << std::endl;
 
