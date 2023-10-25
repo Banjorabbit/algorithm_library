ddd09667dc67a529cab67f48d14ea4319739b252
banjorabbit <kristiantimm@gmail.com>
2023-10-25 19:29:09 +0200

    add test when changing cutoff frequency

--- apps/test/src/test_dc_remover_first_order.cpp
+++ apps/test/src/test_dc_remover_first_order.cpp
@@ -10,3 +10,29 @@ TEST(DCRemover, Interface)
 {
 	EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<DCRemoverFirstOrder>());
 }
+
+TEST(DCRemover, changecutoff)
+{
+	auto c = DCRemoverFirstOrder::Coefficients();
+	c.nChannels = 1;
+	DCRemoverFirstOrder dcRemover(c);
+
+	auto p = dcRemover.getParameters();
+	p.cutoffFrequency = 100.f;
+	dcRemover.setParameters(p);
+
+	int n = 1000;
+	Eigen::ArrayXf input(n), output(n), output2(n);
+	input.setRandom();
+
+	dcRemover.process(input, output);
+	
+	p.cutoffFrequency = 10000.f;
+	dcRemover.setParameters(p);
+	dcRemover.process(input, output2);
+	dcRemover.process(input, output2);
+
+	float difference = (output - output2).abs2().sum() / output.abs2().sum();
+	fmt::print("Relative difference: {}\n", difference);
+	EXPECT_TRUE(difference > .4f);
+}
