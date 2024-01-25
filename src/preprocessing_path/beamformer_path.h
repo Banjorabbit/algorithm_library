4397e843910b256ca347ec991dda30efaefc47f3
banjorabbit <kristiantimm@gmail.com>
2024-01-25 01:16:19 +0100

    add DCRemover to beamformer path

--- src/preprocessing_path/beamformer_path.h
+++ src/preprocessing_path/beamformer_path.h
@@ -4,12 +4,14 @@
 #include "filterbank/filterbank_wola.h"
 #include "beamformer/beamformer_mvdr.h"
 #include "activity_detection/activity_detection_noise_estimation.h"
+#include "dc_remover/dc_remover_first_order.h"
 
 class BeamformerPath : public AlgorithmImplementation<PreprocessingPathConfiguration, BeamformerPath>
 {
 public:
     BeamformerPath(const Coefficients& c = Coefficients()) :
-        AlgorithmImplementation<PreprocessingPathConfiguration, BeamformerPath>{c}
+        AlgorithmImplementation<PreprocessingPathConfiguration, BeamformerPath>{c},
+        dcRemover({c.nChannels, c.sampleRate})
     {
         filterbank.setStandardFilterbank(c.bufferSize, c.nChannels);
         filterbankInverse.setStandardFilterbank(c.bufferSize, 1); // only output 1 channel
@@ -32,14 +34,17 @@ public:
     FilterbankSynthesisWOLA filterbankInverse;
     BeamformerMVDR beamformer;
     ActivityDetectionFusedNoiseEstimation activityDetector;
-    DEFINE_MEMBER_ALGORITHMS(filterbank, filterbankInverse, beamformer, activityDetector)
+    DCRemoverFirstOrder dcRemover;
+    DEFINE_MEMBER_ALGORITHMS(filterbank, filterbankInverse, beamformer, activityDetector, dcRemover)
 
     void processOn(Input input, Output output)
 	{
+        Eigen::ArrayXXf xTime(C.bufferSize, C.nChannels);
         Eigen::ArrayXXcf xFreq(nBands, C.nChannels);
         Eigen::ArrayXcf xBeamformed(nBands);
         bool activity;
-		filterbank.process(input, xFreq);
+        dcRemover.process(input, xTime);
+		filterbank.process(xTime, xFreq);
         activityDetector.process(xFreq.abs2(), activity);
         beamformer.process({xFreq, activity}, xBeamformed); 
         filterbankInverse.process(xBeamformed, output);
