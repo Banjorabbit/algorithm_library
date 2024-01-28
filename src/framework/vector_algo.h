2f0d3a579e6dcbb2f2ec9224e587140475d01b66
banjorabbit <kristiantimm@gmail.com>
2024-01-28 21:49:07 +0100

    updated VectorAlgo to new framework

--- src/framework/vector_algo.h
+++ src/framework/vector_algo.h
@@ -7,237 +7,133 @@
 // author: Kristian Timm Andersen, 2019
 
 template<typename Talgo>
-class VectorAlgoStatic
-{
-public:
-
-	static constexpr size_t VECTOR_ALGORITHM_VERSION_MINOR = 1; // version changes in implementation
-
-	size_t getDynamicMemorySize() const
-	{
-		size_t size = 0;
-		for (auto& element : vec)
-		{
-			size += element.getDynamicMemorySize() + element.getStaticMemorySize(); // static memory is included since it is dynamically allocated in std::vec<Talgo>
-		}
-		return size;
-	}
-
-	static size_t getStaticMemorySize() { return sizeof(VectorAlgoStatic<Talgo>); }
-
-	template<typename Tcoefficients>
-	bool initialize(const Tcoefficients& c)
-	{
-		bool flag = true;
-		for (auto& element : vec) { flag &= element.initialize(c); }
-		return flag;
-	}
-
-	template<typename Tcoefficients>
-	bool initialize(const std::vector<Tcoefficients>& c)
-	{
-		bool flag = true;
-		for (auto i = 0; i < c.size(); i++)
-		{
-			flag &= vec[i].initialize(c[i]);
-		}
-		return flag;
-	}
-
-	bool initialize()
-	{
-		bool flag = true;
-		for (auto& element : vec) { flag &= element.initialize(); }
-		return flag;
-	}
-
-	// get
-	std::vector<typename Talgo::Coefficients> getCoefficients() const
-	{
-		std::vector<typename Talgo::Coefficients> cVec(vec.size());
-		for (auto i = 0; i < vec.size(); i++) { cVec[i] = vec[i].getCoefficients(); }
-		return cVec;
-	}
-
-	// set
-	void setCoefficients(const typename Talgo::Coefficients& c) { for (size_t i = 0;i < size();i++) { vec[i].setCoefficients(c); } }
-
-	template<typename Tcoefficients>
-	void setCoefficients(const Tcoefficients& c) { for (size_t i = 0; i < size(); i++) { vec[i].setCoefficients(c[i]); } }
-
-	auto getCoefficientsAll() const
-	{
-		std::vector<decltype(vec[0].getCoefficientsAll())> cVec(vec.size());
-		for (size_t i = 0; i < vec.size(); i++) { cVec[i] = vec[i].getCoefficientsAll(); }
-		return cVec;
-	}
-
-	template<typename Tcoefficients>
-	void setCoefficientsAll(const Tcoefficients& c)
-	{
-		if (vec.size() != c.size()) { resize(c.size()); }
-		for (size_t i = 0; i < size(); i++)
-		{
-			vec[i].setCoefficientsAll(c[i]);
-		}
-	}
-
-	inline void resize(const size_t i) { vec = std::vector<Talgo>(i); } // algorithms have no copy or move constructor so resizing can only be done by allocating new vector
-	inline size_t size() const { return vec.size(); }
-	Talgo& operator[](const size_t i) { return vec[i]; }
-	auto begin() { return vec.begin(); }
-	auto begin() const { return vec.begin(); }
-	auto end() { return vec.end(); }
-	auto end()   const { return vec.end(); }
-
-	VectorAlgoStatic() = default;
-	~VectorAlgoStatic() = default;
-	VectorAlgoStatic(typename Talgo::Coefficients c) { setCoefficients(c); }
-
-protected:
-	std::vector<Talgo> vec;
-};
-
-template<typename Talgo>
-class VectorAlgoSimple : public VectorAlgoStatic<Talgo>
-{
-public:
-	void reset() { for (auto& element : VectorAlgoStatic<Talgo>::vec) { element.reset(); } }
-
-	VectorAlgoSimple() = default;
-	~VectorAlgoSimple() = default;
-	VectorAlgoSimple(typename Talgo::Coefficients c) : VectorAlgoStatic<Talgo>{ c } {}
-};
-
-
-template<typename Talgo>
 class VectorAlgo
 {
 public:
-
 	static constexpr size_t VECTOR_ALGORITHM_VERSION_MINOR = 1; // version changes in implementation
+	
+	using Coefficients = typename Talgo::Coefficients;
+	using Parameters = typename Talgo::Parameters;
+	using Setup = typename Talgo::Setup;
 
-	size_t getDynamicMemorySize() const
+	VectorAlgo() = default;
+	VectorAlgo(int size) : vec(size) {} // initialize with size
+	VectorAlgo(int size, const Coefficients& c) : vec(size, c) {} 	// intitialize with size and Coefficients c
+	VectorAlgo(int size, const Setup& s) : vec(size, s) {} 		// initialize with size and Setup S
+	VectorAlgo(const std::vector<Coefficients>& c) : vec(c.begin(), c.end()) {} 	// initialize with vector of Coefficients
+	VectorAlgo(const std::vector<Setup>& s) : vec(s.begin(), s.end()) {} 			// initialize with vector of Setups
+	~VectorAlgo() = default;
+
+	size_t getDynamicSize() const
 	{
 		size_t size = 0;
 		for (auto& element : vec)
 		{
-			size += element.getDynamicMemorySize() + element.getStaticMemorySize(); // static memory is included since it is dynamically allocated in std::vec<Talgo>
+			size += element.getDynamicSize() + element.getStaticSize(); // static memory is included since it is dynamically allocated in std::vec<Talgo>
 		}
 		return size;
 	}
 
-	static size_t getStaticMemorySize() { return sizeof(VectorAlgo); }
-
-	template<typename Tcoefficients>
-	bool initialize(const Tcoefficients& c)
-	{
-		bool flag = true;
-		for (auto& element : vec) { flag &= element.initialize(c); }
-		return flag;
-	}
-
-	template<typename Tcoefficients>
-	bool initialize(const std::vector<Tcoefficients>& c)
-	{
-		bool flag = true;
-		for (auto i = 0; i < c.size(); i++)
-		{
-			flag &= vec[i].initialize(c[i]);
-		}
-		return flag;
-	}
-
-	bool initialize()
-	{
-		bool flag = true;
-		for (auto& element : vec) { flag &= element.initialize(); }
-		return flag;
-	}
+	static size_t getStaticSize() { return sizeof(VectorAlgo); }
 
 	void reset() { for (auto& element : vec) { element.reset(); } }
 
 	// get
-	std::vector<typename Talgo::Coefficients> getCoefficients() const
+	std::vector<Coefficients> getCoefficients() const
 	{
-		std::vector<typename Talgo::Coefficients> cVec(vec.size());
+		std::vector<Coefficients> cVec(vec.size());
 		for (auto i = 0; i < vec.size(); i++) { cVec[i] = vec[i].getCoefficients(); }
 		return cVec;
 	}
 
-	std::vector<typename Talgo::Parameters> getParameters() const
+	std::vector<Parameters> getParameters() const
 	{
-		std::vector<typename Talgo::Parameters> pVec(vec.size());
+		std::vector<Parameters> pVec(vec.size());
 		for (auto i = 0; i < vec.size(); i++) { pVec[i] = vec[i].getParameters(); }
 		return pVec;
 	}
 
-	std::vector<typename Talgo::Setup> getSetup() const
+	std::vector<Setup> getSetup() const
 	{
-		std::vector<typename Talgo::Setup> sVec(vec.size());
+		std::vector<Setup> sVec(vec.size());
 		for (auto i = 0; i < vec.size(); i++) { sVec[i] = vec[i].getSetup(); }
 		return sVec;
 	}
 
-	auto getCoefficientsAll() const
+	auto getCoefficientsTree() const
 	{
-		std::vector<decltype(vec[0].getCoefficientsAll())> cVec(vec.size());
-		for (size_t i = 0; i < vec.size(); i++) { cVec[i] = vec[i].getCoefficientsAll(); }
+		std::vector<decltype(vec[0].getCoefficientsTree())> cVec(vec.size());
+		for (size_t i = 0; i < vec.size(); i++) { cVec[i] = vec[i].getCoefficientsTree(); }
 		return cVec;
 	}
 
-	auto getParametersAll() const
+	auto getParametersTree() const
 	{
-		std::vector<decltype(vec[0].getParametersAll())> pVec(vec.size());
-		for (size_t i = 0; i < vec.size(); i++) { pVec[i] = vec[i].getParametersAll(); }
+		std::vector<decltype(vec[0].getParametersTree())> pVec(vec.size());
+		for (size_t i = 0; i < vec.size(); i++) { pVec[i] = vec[i].getParametersTree(); }
 		return pVec;
 	}
 
-	auto getSetupAll() const
+	auto getSetupTree() const
 	{
-		std::vector<decltype(vec[0].getSetupAll())> sVec(vec.size());
-		for (size_t i = 0; i < vec.size(); i++) { sVec[i] = vec[i].getSetupAll(); }
+		std::vector<decltype(vec[0].getSetupTree())> sVec(vec.size());
+		for (size_t i = 0; i < vec.size(); i++) { sVec[i] = vec[i].getSetupTree(); }
 		return sVec;
 	}
 
 	// set
+	void setCoefficients(const Coefficients& c) { for (size_t i = 0;i < size();i++) { vec[i].setCoefficients(c); } }
+
 	template<typename Tcoefficients>
-	void setCoefficients(const Tcoefficients& c) { for (size_t i = 0; i < size(); i++) { vec[i].setCoefficients(c[i]); } }
+	void setCoefficients(const Tcoefficients& c) 
+	{ 
+		if (c.size() != size()) { resize(c.size); } 
+		for (size_t i = 0; i < size(); i++) { vec[i].setCoefficients(c[i]); } 
+	}
+
+	void setParameters(const Parameters& p) { for (size_t i = 0;i < size();i++) { vec[i].setParameters(p); } }
 
 	template<typename Tparameters>
-	void setParameters(const Tparameters& p) { for (size_t i = 0; i < size(); i++) { vec[i].setParameters(p[i]); } }
+	void setParameters(const Tparameters& p) 
+	{ 
+		if (p.size() != size()) { resize(p.size()); }
+		for (size_t i = 0; i < size(); i++) { vec[i].setParameters(p[i]); } 
+	}
+
+	void setSetup(const Setup& s) { for (size_t i = 0;i < size();i++) { vec[i].setSetup(s); } }
 
 	template<typename Tsetup>
-	void setSetup(const Tsetup& setup) { for (size_t i = 0; i < size(); i++) { vec[i].setSetup(setup[i]); } }
+	void setSetup(const Tsetup& s) 
+	{ 
+		if (s.size() != size()) { resize(s.size()); }
+		for (size_t i = 0; i < size(); i++) { vec[i].setSetup(s[i]); } 
+	}
 
 	template<typename Tcoefficients>
-	void setCoefficientsAll(const Tcoefficients& c)
+	void setCoefficientsTree(const Tcoefficients& c)
 	{
 		if (vec.size() != c.size()) { resize(c.size()); }
 		for (size_t i = 0; i < size(); i++)
 		{
-			vec[i].setCoefficientsAll(c[i]);
+			vec[i].setCoefficientsTree(c[i]);
 		}
 	}
 
 	template<typename Tparameters>
-	void setParametersAll(const Tparameters& p)
+	void setParametersTree(const Tparameters& p)
 	{
-		if (vec.size() != p.size())
-		{
-			resize(p.size());
-			uninitialize();
-		}
-		for (size_t i = 0; i < size(); i++) { vec[i].setParametersAll(p[i]); }
+		if (vec.size() != p.size()) { resize(p.size()); }
+		for (size_t i = 0; i < size(); i++) { vec[i].setParametersTree(p[i]); }
 	}
+
 	template<typename Tsetup>
-	void setSetupAll(const Tsetup& s) { resize(s.size()); for (size_t i = 0; i < size(); i++) { vec[i].setSetupAll(s[i]); } }
+	void setSetupTree(const Tsetup& s) 
+	{ 
+		if (s.size() != size()) { resize(s.size()); }
+		for (size_t i = 0; i < size(); i++) { vec[i].setSetupTree(s[i]); } 
+	}
 
-	void disable() { for (auto& element : vec) { element.disable(); } }
-	void enable() { for (auto& element : vec) { element.enable(); } }
-	void uninitialize() { for (auto& element : vec) { element.uninitialize(); } }
-
-	inline void resize(const size_t i) { vec = std::vector<Talgo>(i); } // algorithms have no copy or move constructor so resizing can only be done by allocating new vector
+	inline void resize(const size_t i) { vec = std::vector<Talgo>(i); } // algorithms might not have a copy or move constructor so resizing can only be done by allocating new vector
 	inline size_t size() const { return vec.size(); }
 	Talgo& operator[](const size_t i) { return vec[i]; }
 	auto begin() { return vec.begin(); }
@@ -245,21 +141,6 @@ public:
 	auto end() { return vec.end(); }
 	auto end()   const { return vec.end(); }
 
-	auto getInitialized() const
-	{
-		std::vector<decltype(Talgo::getInitialized())> iVec(vec.size());
-		for (auto i = 0; i < vec.size(); i++) { iVec[i] = vec[i].initialized(); }
-		return iVec;
-	}
-
-	auto getEnabled() const
-	{
-		std::vector<decltype(Talgo::getEnabled())> gVec(vec.size());
-		for (auto i = 0; i < vec.size(); i++) { gVec[i] = vec[i].getEnabled(); }
-		return gVec;
-	}
-
-	static constexpr size_t STATIC_VECTOR_ALGO_VERSION = 1; // version changes in ABI
 private:
-	std::vector<Talgo> vec;
+	std::vector<Talgo> vec;	
 };
