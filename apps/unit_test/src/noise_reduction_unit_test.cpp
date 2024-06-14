f51bf86e3cbf35720dd2d7df51b7a98118b8a831
Kristian office <kristiantimm@gmail.com>
2024-06-14 15:00:05 +0200

    add interface test of ML noise reduction

--- apps/unit_test/src/noise_reduction_unit_test.cpp
+++ apps/unit_test/src/noise_reduction_unit_test.cpp
@@ -1,4 +1,5 @@
 #include "noise_reduction/noise_reduction_apriori.h"
+#include "noise_reduction/noise_reduction_ml.h"
 #include "unit_test.h"
 #include "gtest/gtest.h"
 
@@ -6,12 +7,6 @@ using namespace Eigen;
 
 // --------------------------------------------- TEST CASES ---------------------------------------------
 
-TEST(NoiseReduction, Interface) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<NoiseReductionAPriori>()); }
+TEST(NoiseReduction, InterfaceApriori) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<NoiseReductionAPriori>()); }
 
-// test that ML version of NoiseReduction can be instantiated
-TEST(NoiseReduction, instantiateML)
-{
-    auto c = NoiseReductionConfiguration::Coefficients();
-    c.algorithmType = NoiseReductionConfiguration::Coefficients::ML;
-    NoiseReduction noiseReduction(c);
-}
+TEST(NoiseReduction, InterfaceML) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<NoiseReductionML>()); }
