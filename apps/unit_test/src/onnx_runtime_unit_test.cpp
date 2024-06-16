202cc5c374444d734036e4b58bae4e4e1c2c56f9
banjorabbit <kristiantimm@gmail.com>
2024-06-16 22:48:56 +0200

    add test that runs ImageClasifier.onnx model

--- apps/unit_test/src/onnx_runtime_unit_test.cpp
+++ apps/unit_test/src/onnx_runtime_unit_test.cpp
@@ -60,7 +60,7 @@ int calculateProduct(const std::vector<std::int64_t> &v)
     return total;
 }
 
-TEST(ONNXRUNTIME, RunONNXModel)
+TEST(ONNXRUNTIME, RunEmptyONNXModel)
 {
     Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "test");
     Ort::Session session(env, "model.onnx", Ort::SessionOptions());
@@ -101,4 +101,83 @@ TEST(ONNXRUNTIME, RunONNXModel)
     std::cout << "Running inference..." << std::endl;
     auto output_tensors = session.Run(Ort::RunOptions{nullptr}, &inputNamePtr, &tensor, nInput, &outputNamePtr, nOutput);
     std::cout << "Inference done." << std::endl;
+}
+
+// test that the ImageClassifier.onnx model can be loaded and run
+TEST(ONNXRUNTIME, RunMLModel)
+{
+    const int nBands = 257;
+
+    Ort::MemoryInfo mem_info = Ort::MemoryInfo::CreateCpu(OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault);
+
+    size_t maxMemory = 0;           // 0 = default
+    int arenaExtendStrategy = -1;   // -1 = default
+    int initialChunkSizeBytes = -1; // -1 = default
+    int maxDeadBytesPerChunk = -1;  // -1 = default
+    Ort::ArenaCfg arenaCfg = Ort::ArenaCfg(maxMemory, arenaExtendStrategy, initialChunkSizeBytes, maxDeadBytesPerChunk);
+
+    Ort::ThreadingOptions threadOptions;
+    threadOptions.SetGlobalInterOpNumThreads(2);
+    Ort::Env env(threadOptions, ORT_LOGGING_LEVEL_WARNING, "global_environment");
+
+    env.CreateAndRegisterAllocator(mem_info, arenaCfg);
+    env.DisableTelemetryEvents();
+
+    Ort::SessionOptions sessionOptions;
+    sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
+    sessionOptions.DisablePerSessionThreads();
+    sessionOptions.DisableProfiling();
+    sessionOptions.AddConfigEntry(kOrtSessionOptionsConfigUseEnvAllocators, "1");
+
+    Ort::Session session{env, "ImageClassifier.onnx", sessionOptions};
+
+    std::vector<std::string> inputNames = {"magnitude", "phase", "time state", "gru1 state", "gru2 state"};
+    std::vector<const char *> inputNamesChar;
+    std::vector<std::string> outputNames = {"gain", "time state out", "gru1 state out", "gru2 state out"};
+    std::vector<const char *> outputNamesChar;
+
+    std::vector<std::int64_t> magnitudeShape;
+    std::vector<std::int64_t> phaseShape;
+    std::vector<std::int64_t> timeBufferShape;
+    std::vector<std::int64_t> gru1BufferShape;
+    std::vector<std::int64_t> gru2BufferShape;
+    Eigen::ArrayXf magnitude;
+    Eigen::ArrayXf phase;
+    Eigen::ArrayXXf timeBuffer;
+    Eigen::ArrayXXf gru1Buffer;
+    Eigen::ArrayXXf gru2Buffer;
+    std::vector<Ort::Value> inputTensors;
+
+    inputNamesChar.resize(inputNames.size());
+    std::transform(std::begin(inputNames), std::end(inputNames), std::begin(inputNamesChar), [&](const std::string &str) { return str.c_str(); });
+    outputNamesChar.resize(outputNames.size());
+    std::transform(std::begin(outputNames), std::end(outputNames), std::begin(outputNamesChar), [&](const std::string &str) { return str.c_str(); });
+
+    magnitudeShape = {1, nBands};
+    phaseShape = {1, nBands};
+    timeBufferShape = {10, nBands};
+    gru1BufferShape = {1, 32, 64};
+    gru2BufferShape = {1, 32, 64};
+    magnitude = Eigen::ArrayXf::Zero(nBands);
+    phase = Eigen::ArrayXf::Zero(nBands);
+    timeBuffer = Eigen::ArrayXXf::Zero(nBands, 10);
+    gru1Buffer = Eigen::ArrayXXf::Zero(64, 32);
+    gru2Buffer = Eigen::ArrayXXf::Zero(64, 32);
+
+    inputTensors.emplace_back(Ort::Value::CreateTensor<float>(mem_info, magnitude.data(), nBands, magnitudeShape.data(), magnitudeShape.size()));
+    inputTensors.emplace_back(Ort::Value::CreateTensor<float>(mem_info, phase.data(), nBands, phaseShape.data(), phaseShape.size()));
+    inputTensors.emplace_back(Ort::Value::CreateTensor<float>(mem_info, timeBuffer.data(), 10 * nBands, timeBufferShape.data(), timeBufferShape.size()));
+    inputTensors.emplace_back(Ort::Value::CreateTensor<float>(mem_info, gru1Buffer.data(), 32 * 64, gru1BufferShape.data(), gru1BufferShape.size()));
+    inputTensors.emplace_back(Ort::Value::CreateTensor<float>(mem_info, gru2Buffer.data(), 32 * 64, gru2BufferShape.data(), gru2BufferShape.size()));
+
+    magnitude = Eigen::ArrayXf::Random(nBands).abs2();
+    phase = Eigen::ArrayXf::Random(nBands);
+    std::cout << "processing\n" << std::endl;
+    auto outputTensors =
+        session.Run(Ort::RunOptions{nullptr}, inputNamesChar.data(), inputTensors.data(), inputNamesChar.size(), outputNamesChar.data(), outputNamesChar.size());
+    std::cout << "processing done!\n" << std::endl;
+
+    std::cout << "processing\n" << std::endl;
+    outputTensors = session.Run(Ort::RunOptions{nullptr}, inputNamesChar.data(), inputTensors.data(), inputNamesChar.size(), outputNamesChar.data(), outputNamesChar.size());
+    std::cout << "processing done!\n" << std::endl;
 }
