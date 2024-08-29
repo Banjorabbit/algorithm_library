6daec144f5602b27ec92a0bdaa2ad728a7ccb5ae
banjorabbit <kristiantimm@gmail.com>
2024-08-29 20:55:37 +0200

    add reconstruction test

--- apps/unit_test/src/bandsplit_combine_chebyshev_test.cpp
+++ apps/unit_test/src/bandsplit_combine_chebyshev_test.cpp
@@ -1,4 +1,5 @@
 #include "bandsplit_downsample/bandsplit_downsample_chebyshev.h"
+#include "fft/fft_real.h"
 #include "unit_test.h"
 #include "gtest/gtest.h"
 
@@ -9,3 +10,52 @@ using namespace Eigen;
 TEST(BandsplitDownsampleChebyshev, Interface) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<BandsplitDownsampleChebyshev>()); }
 
 TEST(CombineBandsplitDownsampleChebyshev, Interface) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<CombineBandsplitDownsampleChebyshev>()); }
+
+TEST(CombineBandsplitDownsampleChebyshev, reconstruction)
+{
+    BandsplitDownsampleChebyshev::Coefficients cBandsplit;
+    cBandsplit.nChannels = 2;
+    cBandsplit.nSamples = 999;
+    cBandsplit.resamplingType = cBandsplit.K48HZ_TO_K16HZ;
+    BandsplitDownsampleChebyshev bandsplit(cBandsplit);
+
+    CombineBandsplitDownsampleChebyshev::Coefficients cCombine;
+    cCombine.nChannels = cBandsplit.nChannels;
+    cCombine.nSamples = 333;
+    cCombine.resamplingType = cCombine.K16HZ_TO_K48HZ;
+    CombineBandsplitDownsampleChebyshev combineBandsplit(cCombine);
+
+    Eigen::ArrayXXf x = Eigen::ArrayXXf::Random(cBandsplit.nSamples, cBandsplit.nChannels);
+    Eigen::ArrayXXf yDownsample = Eigen::ArrayXXf::Zero(cCombine.nSamples, cCombine.nChannels);
+    Eigen::ArrayXXf yHighFrequency = Eigen::ArrayXXf::Zero(cBandsplit.nSamples, cCombine.nChannels);
+    Eigen::ArrayXXf y = Eigen::ArrayXXf::Zero(cBandsplit.nSamples, cBandsplit.nChannels);
+
+    bandsplit.process(x, {yDownsample, yHighFrequency});
+    combineBandsplit.process({yDownsample, yHighFrequency}, y);
+
+    // calculate spectrum of output signals and calculate output error
+    const int delay = 13;                // delay between input/output
+    int n = cBandsplit.nSamples - delay; // length of test signal
+
+    // init FFT
+    FFTReal::Coefficients cFFT;
+    cFFT.fftSize = FFTReal::Configuration::getValidFFTSize(cBandsplit.nSamples);
+    FFTReal fft(cFFT);
+
+    // init FFT arrays
+    Eigen::ArrayXXf xZeropad = Eigen::ArrayXXf::Zero(cFFT.fftSize, cBandsplit.nChannels);
+    xZeropad.topRows(n) = x.topRows(n);
+    Eigen::ArrayXXf yZeropad = Eigen::ArrayXXf::Zero(cFFT.fftSize, cBandsplit.nChannels);
+    yZeropad.topRows(n) = y.bottomRows(n);
+
+    Eigen::ArrayXXcf X(FFTReal::Configuration::convertFFTSizeToNBands(cFFT.fftSize), cBandsplit.nChannels);
+    Eigen::ArrayXXcf Y(FFTReal::Configuration::convertFFTSizeToNBands(cFFT.fftSize), cBandsplit.nChannels);
+
+    // calculate FFT and print error
+    fft.process(xZeropad, X);
+    fft.process(yZeropad, Y);
+
+    float error = (X.abs() - Y.abs()).abs2().sum() / X.abs2().sum();
+    fmt::print("Output error: {}\n", error);
+    ASSERT_TRUE(error < 0.005);
+}
