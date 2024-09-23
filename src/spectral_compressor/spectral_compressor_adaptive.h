341bea89699348745eb846f3ee2057f867bbfb3a
banjorabbit <kristiantimm@gmail.com>
2024-09-23 23:23:19 +0200

    add delay to short and medium compressor to make them aligned

--- src/spectral_compressor/spectral_compressor_adaptive.h
+++ src/spectral_compressor/spectral_compressor_adaptive.h
@@ -13,7 +13,10 @@ class SpectralCompressorAdaptive : public AlgorithmImplementation<SpectralCompre
 {
   public:
     SpectralCompressorAdaptive(Coefficients c = {.nChannels = 2, .sampleRate = 48000.f, .bufferSize = 4096})
-        : BaseAlgorithm{c}, spectralCompressorShort({.nChannels = c.nChannels, .sampleRate = c.sampleRate, .bufferSize = c.bufferSize / 16}),
+        : BaseAlgorithm{c}, 
+          delayShort({.nChannels = c.nChannels, .delayLength = 3 * (c.bufferSize - c.bufferSize / 16)}),
+          delayMedium({.nChannels = c.nChannels, .delayLength = 3 * (c.bufferSize - c.bufferSize / 4)}), 
+          spectralCompressorShort({.nChannels = c.nChannels, .sampleRate = c.sampleRate, .bufferSize = c.bufferSize / 16}),
           spectralCompressorMedium({.nChannels = c.nChannels, .sampleRate = c.sampleRate, .bufferSize = c.bufferSize / 4}),
           spectralCompressorLong({.nChannels = c.nChannels, .sampleRate = c.sampleRate, .bufferSize = c.bufferSize}),
           spectralSelector({.nChannels = 3 * c.nChannels, .nStreams = 3, .bufferSize = c.bufferSize / 16})
@@ -22,27 +25,33 @@ class SpectralCompressorAdaptive : public AlgorithmImplementation<SpectralCompre
         resetVariables();
     }
 
+    CircularBuffer delayShort;
+    CircularBuffer delayMedium;
     SpectralCompressorWOLA spectralCompressorShort;
     SpectralCompressorWOLA spectralCompressorMedium;
     SpectralCompressorWOLA spectralCompressorLong;
     SpectralSelector spectralSelector;
 
-    DEFINE_MEMBER_ALGORITHMS(spectralCompressorShort, spectralCompressorMedium, spectralCompressorLong, spectralSelector)
+    DEFINE_MEMBER_ALGORITHMS(delayShort, delayMedium, spectralCompressorShort, spectralCompressorMedium, spectralCompressorLong, spectralSelector)
 
     float getDelaySamples() const { return static_cast<float>(C.bufferSize * 3); }
 
   private:
     void inline processAlgorithm(Input input, Output output)
     {
+        delayMedium.process(input, spectralBuffer.middleCols(C.nChannels, C.nChannels)); // delay so delayMedium + spectralCompressorMedium has delay equal to spectralCompressorLong
+        delayShort.process(input, spectralBuffer.rightCols(C.nChannels)); // delay so delayShort + spectralCompressorShort has delay equal to spectralCompressorLong
         spectralCompressorLong.process(input, spectralBuffer.leftCols(C.nChannels));
         for (auto iMedium = 0; iMedium < 4; iMedium++)
         {
-            spectralCompressorMedium.process(input.middleRows(iMedium * C.bufferSize / 4, C.bufferSize / 4),
+            // spectralCompressor can share input/output buffer
+            spectralCompressorMedium.process(spectralBuffer.block(iMedium * C.bufferSize / 4, C.nChannels, C.bufferSize / 4, C.nChannels),
                                              spectralBuffer.block(iMedium * C.bufferSize / 4, C.nChannels, C.bufferSize / 4, C.nChannels));
             for (auto iShort = 0; iShort < 4; iShort++)
             {
+                // spectralCompressor can share input/output buffer
                 spectralCompressorShort.process(
-                    input.middleRows(iMedium * C.bufferSize / 4 + iShort * C.bufferSize / 16, C.bufferSize / 16),
+                    spectralBuffer.block(iMedium * C.bufferSize / 4 + iShort * C.bufferSize / 16, 2 * C.nChannels, C.bufferSize / 16, C.nChannels),
                     spectralBuffer.block(iMedium * C.bufferSize / 4 + iShort * C.bufferSize / 16, 2 * C.nChannels, C.bufferSize / 16, C.nChannels));
 
                 spectralSelector.process(spectralBuffer.middleRows(iMedium * C.bufferSize / 4 + iShort * C.bufferSize / 16, C.bufferSize / 16),
