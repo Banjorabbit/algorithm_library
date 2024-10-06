158f75a0df8c630fad2925cf556823e9b0be858b
banjorabbit <kristiantimm@gmail.com>
2024-10-06 13:21:07 +0200

    add test for public IIRFilter class to test valid in/output methods

--- apps/unit_test/src/iir_filter_unit_test.cpp
+++ apps/unit_test/src/iir_filter_unit_test.cpp
@@ -9,3 +9,15 @@ using namespace Eigen;
 TEST(IIRFilter, Interface2ndOrder) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<IIRFilter2ndOrder>()); }
 
 TEST(IIRFilter, InterfaceCascaded) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<IIRFilterCascaded>()); }
+
+// test the valid input/output methods for the public class IIRFilter
+TEST(IIRFilter, publicValidinputOutput)
+{
+    IIRFilter filter;
+    auto input = filter.initInput();
+    auto output = filter.initOutput(input);
+    filter.process(input, output);
+
+    EXPECT_TRUE(filter.validInput(input));
+    EXPECT_TRUE(filter.validOutput(output));
+}
