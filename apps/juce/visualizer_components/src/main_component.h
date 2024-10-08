1d3c5c7584e3ba39be9f585b2c6773c86ddd9d6d
Banjorabbit <kristiantimm@gmail.com>
2024-10-08 16:36:59 +0200

    inherit from AudioAppComponent and implement needed methods

--- apps/juce/visualizer_components/src/main_component.h
+++ apps/juce/visualizer_components/src/main_component.h
@@ -2,11 +2,11 @@
 #include <juce_gui_basics/juce_gui_basics.h>
 #include "spectrogram_component.h"
 
-class MainComponent final : public juce::Component
+class MainComponent final : public juce::AudioAppComponent
 {
 public:
     //==============================================================================
-    MainComponent() : spectrogram(256)
+    MainComponent() :spectrogram(48000.f)
     {
         setSize (750, 500);
         //addAndMakeVisible (fileReaderComponent);
@@ -24,9 +24,23 @@ public:
 
     void resized() override {}
 
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override 
+    {
+        spectrogram.prepareToPlay(samplesPerBlockExpected, static_cast<float>(sampleRate));
+    }
+
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override 
+    {
+        Eigen::Map<const Eigen::ArrayXf> array(bufferToFill.buffer->getReadPointer(0, bufferToFill.startSample), bufferToFill.numSamples);
+        spectrogram.pushSamples(array);
+    }
+
+    void releaseResources() override { }
+
 private:
     //AudioFileReaderComponent<ConvolutionDSP> fileReaderComponent;
     SpectrogramComponent spectrogram;
+    juce::Image spectrogramImage;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
--- apps/juce/visualizer_components/src/spectrogram_component.h
+++ apps/juce/visualizer_components/src/spectrogram_component.h
@@ -1,14 +1,78 @@
 #pragma once
 #include <juce_gui_basics/juce_gui_basics.h>
 #include "algorithm_library/spectrogram.h"
+#include <juce_audio_utils/juce_audio_utils.h>
 
 class SpectrogramComponent : public juce::Component
 {
 public:
-    SpectrogramComponent(int bufferSize) : spectrogram({.bufferSize = bufferSize, .nBands = bufferSize-1, .algorithmType = SpectrogramConfiguration::Coefficients::HANN})
-    { }
+    SpectrogramComponent(float sampleRate) : 
+        bufferSize(getBufferSize(sampleRate)),
+        spectrogram({.bufferSize = bufferSize, .nBands = bufferSize+1, .algorithmType = SpectrogramConfiguration::Coefficients::HANN})
+    { 
+        circularBuffer = Eigen::ArrayXf::Zero(getcircularBufferSize(static_cast<int>(.01*sampleRate), sampleRate));
+        circularBufferIndex.store(0);
+    }
+
+    void prepareToPlay(int expectedBufferSize, float sampleRate)
+    {
+        int bufferSizeNew = getBufferSize(sampleRate);
+        if (bufferSizeNew != bufferSize)
+        {
+            auto c = spectrogram.getCoefficients();
+            c.bufferSize = bufferSize;
+            c.nBands = bufferSize+1;
+            spectrogram.setCoefficients(c);
+            bufferSize = bufferSizeNew;
+        }
+        int circularBufferSize = getcircularBufferSize(expectedBufferSize, sampleRate); 
+        if (circularBufferSize != circularBuffer.size())
+        {
+            circularBuffer = Eigen::ArrayXf::Zero(circularBufferSize);
+            circularBufferIndex.store(0);
+        }
+    }
+
+    // push buffer into circular buffer. This method is likely to be called from the main audio thread and should not do any heavy calculations or GUI work
+    void pushSamples(I::Real buffer)
+    {
+        int index = circularBufferIndex.load();
+        int size = static_cast<int>(buffer.size());
+        int sizeCircular = static_cast<int>(circularBuffer.size());
+        if (size > sizeCircular) // fallback if given more samples than we can handle
+        {
+            index = 0;
+            size = sizeCircular;
+        }
+        
+        int size1 = std::min(sizeCircular - index, size);
+        int size2 = size - size1;
+        circularBuffer.segment(index, size1) = buffer.head(size1);
+        circularBuffer.head(size2) = buffer.tail(size2);
+
+        int indexNew = index + size;
+        if (indexNew >= sizeCircular) { circularBufferIndex.store(indexNew - sizeCircular); }
+        else { circularBufferIndex.store(indexNew); }
+    }
+
+    void reset()
+    {
+        circularBuffer.setZero();
+        circularBufferIndex.store(0);
+        spectrogram.reset();
+    }
 
 private:
+    // bufferSize is around 10ms and half the number of samples in the FFT with 50% overlap
+    int getBufferSize(float sampleRate) const { return SpectrogramConfiguration::getValidFFTSize(static_cast<int>(2 * sampleRate * 0.01f)) / 2; }
+
+    // circular buffer size is max of 100ms and 4x the expected buffer size
+    int getcircularBufferSize(int expectedBufferSize, float sampleRate) const { return std::max(static_cast<int>(sampleRate * 0.1f), 4 * expectedBufferSize); }
+
+    int bufferSize;
+    Eigen::ArrayXf circularBuffer;
+    std::atomic<int> circularBufferIndex;
     Spectrogram spectrogram;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SpectrogramComponent)
 };
