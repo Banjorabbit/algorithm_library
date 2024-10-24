a3cc608754d00311f3cd5b5536682482e80571ed
banjorabbit <kristiantimm@gmail.com>
2024-10-24 22:18:12 +0200

    add test for print out delays of filterbanks

--- apps/unit_test/src/filterbank_set_unit_test.cpp
+++ apps/unit_test/src/filterbank_set_unit_test.cpp
@@ -1,4 +1,5 @@
 #include "filterbank_set/filterbank_set_wola.h"
+#include "fmt/ranges.h"
 #include "unit_test.h"
 #include "gtest/gtest.h"
 
@@ -7,3 +8,18 @@ using namespace Eigen;
 // --------------------------------------------- TEST CASES ---------------------------------------------
 
 TEST(FilterbankSetAnalysis, Interface) { EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<FilterbankSetAnalysisWOLA>()); }
+
+TEST(FilterbankSetAnalysis, MeasureDelay)
+{
+    FilterbankSetAnalysisWOLA filterbankSet;
+
+    auto c = filterbankSet.getCoefficients();
+
+    std::vector<int> delays(c.nFilterbanks);
+    for (auto i = 0; i < c.nFilterbanks; i++)
+    {
+        delays[i] = static_cast<int>(std::round(filterbankSet.filterbanks[i].getDelaySamples()));
+    }
+
+    fmt::print("Delays: {} samples\n", fmt::join(delays, ", "));
+}
