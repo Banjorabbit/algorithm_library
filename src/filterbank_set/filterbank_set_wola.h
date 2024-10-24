491d1fae55892de536e8f4a54c2473b3555a3bdb
banjorabbit <kristiantimm@gmail.com>
2024-10-24 22:18:54 +0200

    replace FilterbankAnalysisWOLA with FilterbankAnalysisSingleChannel

--- src/filterbank_set/filterbank_set_wola.h
+++ src/filterbank_set/filterbank_set_wola.h
@@ -1,21 +1,15 @@
 #pragma once
 #include "algorithm_library/filterbank_set.h"
-#include "filterbank/filterbank_wola.h"
+#include "delay/circular_buffer.h"
+#include "filterbank/filterbank_single_channel.h"
 #include "framework/framework.h"
 
 class FilterbankSetAnalysisWOLA : public AlgorithmImplementation<FilterbankSetAnalysisConfiguration, FilterbankSetAnalysisWOLA>
 {
   public:
-    FilterbankSetAnalysisWOLA(Coefficients c = Coefficients()) : BaseAlgorithm{c}, filterbanks(initializeFilterbanks(c))
-    {
-        bufferSizes.resize(c.nFilterbanks);
-        for (auto i = 0; i < c.nFilterbanks; i++)
-        {
-            bufferSizes[i] = c.bufferSize * (1 << i);
-        }
-    }
+    FilterbankSetAnalysisWOLA(Coefficients c = Coefficients()) : BaseAlgorithm{c}, filterbanks(initializeFilterbanks()) {}
 
-    VectorAlgo<FilterbankAnalysisWOLA> filterbanks;
+    VectorAlgo<FilterbankAnalysisSingleChannel> filterbanks;
     DEFINE_MEMBER_ALGORITHMS(filterbanks)
 
   private:
@@ -23,31 +17,22 @@ class FilterbankSetAnalysisWOLA : public AlgorithmImplementation<FilterbankSetAn
     {
         for (auto iFB = 0; iFB < C.nFilterbanks; iFB++)
         {
-            const int nFrames = input.size() / bufferSizes[iFB];
-            const int bufferSize = bufferSizes[iFB];
-            for (auto iFrame = 0; iFrame < nFrames; iFrame++)
-            {
-                filterbanks[iFB].process(input.segment(iFrame * bufferSize, bufferSize), output[iFB].col(iFrame));
-            }
+            filterbanks[iFB].process(input, output[iFB]);
         }
     }
 
-    std::vector<FilterbankAnalysisWOLA::Coefficients> initializeFilterbanks(const Coefficients &c)
+    std::vector<FilterbankAnalysisWOLA::Coefficients> initializeFilterbanks()
     {
-        std::vector<FilterbankAnalysisWOLA::Coefficients> cFB(c.nFilterbanks);
-        for (auto i = 0; i < c.nFilterbanks; i++)
+        std::vector<FilterbankAnalysisSingleChannel::Coefficients> cFB(C.nFilterbanks);
+        for (auto i = 0; i < C.nFilterbanks; i++)
         {
-            cFB[i].bufferSize = c.bufferSize * (1 << i);
+            cFB[i].bufferSize = C.bufferSize * (1 << i);
             cFB[i].filterbankType = cFB[i].HANN;
             cFB[i].nBands = FFTConfiguration::convertFFTSizeToNBands(4 * cFB[i].bufferSize);
             cFB[i].nChannels = 1;
         }
         return cFB;
     }
-
-    size_t getDynamicSizeVariables() const final { return sizeof(int) * C.nFilterbanks; } // dynamic size of bufferSizes
-
-    std::vector<int> bufferSizes;
 
     friend BaseAlgorithm;
 };
