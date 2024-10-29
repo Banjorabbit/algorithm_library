b15d701722446efdf7067b699ba8a5445de60b67
banjorabbit <kristiantimm@gmail.com>
2024-10-29 23:00:38 +0100

    new algorithm upscale2DLinear

---
+++ apps/unit_test/src/upscale2d_linear.unit_test.cpp
@@ -0,0 +1,36 @@
+#include "spectrogram/upscale2d_linear.h"
+#include "unit_test.h"
+#include "gtest/gtest.h"
+#include <iostream>
+using namespace Eigen;
+
+// --------------------------------------------- TEST CASES ---------------------------------------------
+
+TEST(Upscale2DLinear, Interface)
+{
+    bool testMallocFlag = false;
+    EXPECT_TRUE(InterfaceTests::algorithmInterfaceTest<Upscale2DLinear>(testMallocFlag));
+}
+
+// print output of upscaling. This test requires manual inspection to verify output
+TEST(Upscale2DLinear, printOutput)
+{
+    auto c = Upscale2DLinear::Coefficients();
+    c.factorHorizontal = 3;
+    c.factorVertical = 5;
+    Upscale2DLinear upscale(c);
+
+    Eigen::ArrayXXf input(3, 3);
+    for (auto i = 0; i < 3; i++)
+    {
+        for (auto j = 0; j < 3; j++)
+        {
+            input(i, j) = i + j;
+        }
+    }
+    auto output = upscale.initOutput(input);
+    upscale.process(input, output);
+
+    std::cout << "input: \n" << input << "\n";
+    std::cout << "output: \n" << output << "\n";
+}
---
+++ src/spectrogram/upscale2d_linear.h
@@ -0,0 +1,106 @@
+#pragma once
+#include "algorithm_library/interface/interface.h"
+#include "framework/framework.h"
+
+// Upscale a 2D float array with a factor in horizontal and vertical direction
+//
+// author: Kristian Timm Andersen
+
+struct Upscale2DConfiguration
+{
+    using Input = I::Real2D;
+    using Output = O::Real2D;
+
+    struct Coefficients
+    {
+        int factorHorizontal = 4;
+        int factorVertical = 4;
+        DEFINE_TUNABLE_COEFFICIENTS(factorHorizontal, factorVertical)
+    };
+
+    struct Parameters
+    {
+        DEFINE_NO_TUNABLE_PARAMETERS
+    };
+
+    static Eigen::ArrayXXf initInput(const Coefficients &c)
+    {
+        return Eigen::ArrayXXf::Random(100, 100); // arbitrary number of samples
+    }
+
+    static Eigen::ArrayXXf initOutput(Input input, const Coefficients &c)
+    {
+        return Eigen::ArrayXXf::Zero(c.factorVertical * (input.rows() - 1) + 1, c.factorHorizontal * (input.cols() - 1) + 1);
+    }
+
+    static bool validInput(Input input, const Coefficients &c) { return input.allFinite() && (input.rows() > 0) && (input.cols() > 0); }
+
+    static bool validOutput(Output output, const Coefficients &c)
+    {
+        return output.allFinite() && (output.rows() >= c.factorVertical) && (output.cols() >= c.factorHorizontal);
+    }
+};
+
+// upscale a 2D float array using linear interpolation
+//
+// author: Kristian Timm Andersen
+class Upscale2DLinear : public AlgorithmImplementation<Upscale2DConfiguration, Upscale2DLinear>
+{
+  public:
+    Upscale2DLinear(Coefficients c = Coefficients()) : BaseAlgorithm{c}
+    {
+        interpolateVertical = Eigen::VectorXf::LinSpaced(c.factorVertical, 1, 1.f / c.factorVertical);
+        interpolateHorizontal = Eigen::VectorXf::LinSpaced(c.factorHorizontal, 1, 1.f / c.factorHorizontal);
+    }
+
+  private:
+    void processAlgorithm(Input input, Output output)
+    {
+        const int cols = input.cols();
+        const int colsm1 = cols - 1;
+        const int rows = input.rows();
+        const int rowsm1 = rows - 1;
+        for (auto iCol = 0; iCol < cols; iCol++)
+        {
+            // linear interpolation across rows
+            for (auto iRow = 0; iRow < rowsm1; iRow++)
+            {
+                for (auto i = 0; i < C.factorVertical; i++)
+                {
+                    output(i + iRow * C.factorVertical, iCol * C.factorHorizontal) =
+                        (input(iRow, iCol) - input(iRow + 1, iCol)) * interpolateVertical(i) + input(iRow + 1, iCol);
+                }
+            }
+            output(rowsm1 * C.factorVertical, iCol * C.factorHorizontal) = input(rowsm1, iCol);
+        }
+
+        // linear interpolation across first col
+        output.middleCols(0, C.factorHorizontal) = (output.col(0).matrix() * interpolateHorizontal.transpose()).array();
+        for (auto iCol = 1; iCol < colsm1; iCol++)
+        {
+            for (auto iHor = 1; iHor < C.factorHorizontal; iHor++)
+            {
+                // linear interpolation across cols
+                output.col(iCol * C.factorHorizontal + iHor) = output.col(iCol * C.factorHorizontal) * interpolateHorizontal(iHor);
+                // backwards linear interpolation across cols
+                output.col(iCol * C.factorHorizontal - iHor) += output.col(iCol * C.factorHorizontal + iHor);
+            }
+        }
+        // backwards linear interpolation across last col
+        for (auto iHor = 1; iHor < C.factorHorizontal; iHor++)
+        {
+            output.col((colsm1 - 1) * C.factorHorizontal + iHor) += output.col(colsm1 * C.factorHorizontal) * interpolateHorizontal(C.factorHorizontal - iHor);
+        }
+    }
+
+    size_t getDynamicSizeVariables() const final
+    {
+        size_t size = interpolateVertical.getDynamicMemorySize();
+        size += interpolateHorizontal.getDynamicMemorySize();
+        return size;
+    }
+
+    Eigen::VectorXf interpolateVertical;
+    Eigen::VectorXf interpolateHorizontal;
+    friend BaseAlgorithm;
+};
