bc7a65c0a8659f4ab62ffd102efd28987fb1880e
thinkpad <kristiantimm@gmail.com>
2024-12-05 15:15:04 +0100

    fix test by splitting process call into frames

--- apps/unit_test/src/spectrogram_filterbank_unit_test.cpp
+++ apps/unit_test/src/spectrogram_filterbank_unit_test.cpp
@@ -14,15 +14,18 @@ TEST(SpectrogramFilterbank, getNFrames)
     Spectrogram spec;
     auto c = spec.getCoefficients();
     const int bufferSize = c.bufferSize;
-    const int nSamples = 10000;
-    const int nFrames = Spectrogram::Configuration::getNFrames(nSamples, bufferSize); // test getNFrames() is a static method
+    const int nFrames = 10;
+    const int nSamples = bufferSize * nFrames;
 
     ArrayXf signal(nSamples);
     signal.setRandom();
 
     ArrayXXf output(c.nBands, nFrames + 1); // add one extra frame than needed
     output.setZero();                       // important to set to zero, since we are checking last frame is zero in success criteria
-    spec.process(signal, output);
+    for (auto frame = 0; frame < nFrames; frame++)
+    {
+        spec.process(signal.segment(frame * bufferSize, bufferSize), output.col(frame));
+    }
 
     bool criteria = (!output.leftCols(nFrames).isZero()) && (output.rightCols(1).isZero()); // test criteria that all nFrames are non-zero and last frame is zero
     fmt::print("Criteria: {}\n", criteria);
